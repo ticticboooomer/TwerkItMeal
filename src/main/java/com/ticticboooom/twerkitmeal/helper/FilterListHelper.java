3b1e20abce5bca75f94322433883fb90ca423bbe
LuHeGi <boo122333@gmail.com>
2022-11-12 14:08:13 +0000

    Refactor the code
    Remove unused code
    Update logic to be consistent
    Add readability variables
    Remove bad code from old times

--- src/main/java/com/ticticboooom/twerkitmeal/helper/FilterListHelper.java
+++ src/main/java/com/ticticboooom/twerkitmeal/helper/FilterListHelper.java
@@ -1,36 +1,43 @@
 package com.ticticboooom.twerkitmeal.helper;
 
-import com.ticticboooom.twerkitmeal.config.TwerkConfig;
+import static com.ticticboooom.twerkitmeal.TwerkItMeal.CONFIG;
 
 import java.util.ArrayList;
 import java.util.List;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class FilterListHelper {
-    public static boolean shouldAllow(String key) {
-        if (TwerkConfig.blackList.size() <= 0) {
-            return true;
-        }
 
-        List<String> variations = new ArrayList<>();
-        // entire block RL
-        variations.add(key);
-        // mod id from RL of block
-        variations.add(key.substring(0, key.indexOf(":")));
-        for (String listed : TwerkConfig.blackList) {
-            if (variations.contains(listed)) {
-                return false;
-            }
-        }
+  public static boolean shouldAllow(Block block) {
+    return shouldAllow(ForgeRegistries.BLOCKS.getKey(block).toString());
+  }
 
-        if (!TwerkConfig.useWhitelist) {
-            return true;
-        }
-
-        for (String listed : TwerkConfig.whitelist) {
-            if (variations.contains(listed)){
-                return true;
-            }
-        }
-        return true;
+  public static boolean shouldAllow(String key) {
+    if (CONFIG.getBlacklist().isEmpty()) {
+      return true;
     }
+
+    List<String> variations = new ArrayList<>();
+    // entire block RL
+    variations.add(key);
+    // mod id from RL of block
+    variations.add(key.substring(0, key.indexOf(":")));
+    for (String listed : CONFIG.getBlacklist()) {
+      if (variations.contains(listed)) {
+        return false;
+      }
+    }
+
+    if (!CONFIG.useWhitelist()) {
+      return true;
+    }
+
+    for (String listed : CONFIG.getWhitelist()) {
+      if (variations.contains(listed)) {
+        return true;
+      }
+    }
+    return true;
+  }
 }
